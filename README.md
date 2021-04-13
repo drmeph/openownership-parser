48ade267d459fc32da52a7f2a8f0975922dbeb56
drmeph <kevin.dorfer@gmail.com>
2021-04-13 09:52:50 -0400

    added json parsing

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Parser to import Open Ownership data in to SAP database
 
 Need to create the following tables
+
+
+## Pre-requisite
+* install ijson
+```bash
+pip install ijson
+```
--- openownershiphandler/main.py
+++ openownershiphandler/main.py
@@ -1,8 +1,21 @@
 import sys
+import ijson
 
 from os import path
 
 def main():
     print('Number of arguments:', len(sys.argv), 'arguments.')
     print('Argument List:', str(sys.argv))
-    print('File exists: '+ str(path.exists(sys.argv[1])))
+
+    inputFile = sys.argv[1]
+    print('File exists: '+ str(path.exists(inputFile)))
+
+    """Read Json file"""
+    if not path.exists(inputFile):
+        sys.exit('File does not exist!')
+
+    with open(inputFile, 'r') as f:
+        objects = ijson.items(f, 'item')
+
+        for obj in objects:
+            print(obj)
